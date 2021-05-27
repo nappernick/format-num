d47c3f79931f2732e0a17e7fcf6a6cc52a8b07b4
nappernick <nick@guesthouseshop.com>
2021-05-27 12:14:16 -0400

    Removed AVA, added Jest, converted test

--- test/test.js
+++ test/test.js
@@ -1,47 +1,46 @@
-import test from 'ava'
-import formatNumber from '../'
+const formatNumber = require("..//index")
 
-test('edge cases', (t) => {
-  t.is(formatNumber(NaN), '0', 'NaN => 0')
-  t.is(formatNumber(NaN, { nanZero: false }), 'NaN', 'NaN => NaN')
-  t.is(formatNumber(NaN, { minFraction: 2, maxFraction: 2 }), '0.00', '0 => 0.00 (min, max)')
+test('edge cases', () => {
+  expect(formatNumber(NaN)).toBe('0')
+  expect(formatNumber(NaN, { nanZero: false })).toBe('NaN')
+  expect(formatNumber(NaN, { minFraction: 2, maxFraction: 2 })).toBe('0.00')
 
-  t.is(formatNumber([]), '0', '[] => 0')
-  t.is(formatNumber([3, 4]), '34', '[3, 4] => 34')
+  expect(formatNumber([])).toBe('0')
+  expect(formatNumber([3, 4])).toBe('34')
 
-  t.is(formatNumber(undefined), '0', 'undefined => 0')
-  t.is(formatNumber(null), '0', 'null => 0')
+  expect(formatNumber(undefined)).toBe('0')
+  expect(formatNumber(null)).toBe('0')
 })
 
-test('format numbers', (t) => {
-  t.is(formatNumber(0), '0', '0 => 0')
-  t.is(formatNumber(10000000.15), '10,000,000.15', '10000000.15 => 10,000,000.15')
-  t.is(formatNumber(1.00000004), '1.00000004', '1.00000004 => 1.00000004')
+test('format numbers', () => {
+  expect(formatNumber(0)).toBe('0')
+  expect(formatNumber(10000000.15)).toBe('10,000,000.15')
+  expect(formatNumber(1.00000004)).toBe('1.00000004')
 
   // maybe a bug?
-  t.is(formatNumber(10000000.00000004), '10,000,000', '')
+  expect(formatNumber(10000000.00000004)).toBe('10,000,000')
 
-  t.is(formatNumber(0.0000000000044535), '0.000000000004454')
-  t.is(formatNumber(0.0000000000044535, { maximumSignificantDigits: 2 }), '0.0000000000045')
+  expect(formatNumber(0.0000000000044535)).toBe('0.000000000004454')
+  expect(formatNumber(0.0000000000044535, { maximumSignificantDigits: 2 })).toBe('0.0000000000045')
 
-  t.is(formatNumber(0, { minFraction: 2, maxFraction: 2 }), '0.00', '0 => 0.00 (min, max)')
+  expect(formatNumber(0, { minFraction: 2, maxFraction: 2 })).toBe('0.00')
 })
 
-test('call Intl.NumberFormat correctly', (t) => {
+test('call Intl.NumberFormat correctly', () => {
   global.Intl.NumberFormat = function (locale, opts) {
     this.format = () => ({ locale, opts })
   }
 
-  t.is(formatNumber(0, { maxSignificant: 2 }).opts.maximumSignificantDigits, 2)
-  t.is(formatNumber(0, { maxSignificantDigits: 2 }).opts.maximumSignificantDigits, 2)
-  t.is(formatNumber(0, { maximumSignificant: 2 }).opts.maximumSignificantDigits, 2)
-  t.is(formatNumber(0, { minSignificant: 2 }).opts.minimumSignificantDigits, 2)
-  t.is(formatNumber(0, { minSignificantDigits: 2 }).opts.minimumSignificantDigits, 2)
-  t.is(formatNumber(0, { minimumSignificant: 2 }).opts.minimumSignificantDigits, 2)
-  t.is(formatNumber(0, { maxInteger: 2 }).opts.maximumIntegerDigits, 2)
+  expect(formatNumber(0, { maxSignificant: 2 }).opts.maximumSignificantDigits).toBe(2)
+  expect(formatNumber(0, { maxSignificantDigits: 2 }).opts.maximumSignificantDigits).toBe(2)
+  expect(formatNumber(0, { maximumSignificant: 2 }).opts.maximumSignificantDigits).toBe(2)
+  expect(formatNumber(0, { minSignificant: 2 }).opts.minimumSignificantDigits).toBe(2)
+  expect(formatNumber(0, { minSignificantDigits: 2 }).opts.minimumSignificantDigits).toBe(2)
+  expect(formatNumber(0, { minimumSignificant: 2 }).opts.minimumSignificantDigits).toBe(2)
+  expect(formatNumber(0, { maxInteger: 2 }).opts.maximumIntegerDigits).toBe(2)
 
-  t.deepEqual(formatNumber(0).locale, ['en-US'])
-  t.deepEqual(formatNumber(0).opts, {
+  expect(formatNumber(0).locale).toEqual(['en-US'])
+  expect(formatNumber(0).opts).toEqual({
     nanZero: true,
     locale: 'en-US',
     localeMatcher: 'best fit',
